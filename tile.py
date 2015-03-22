51b945af1f4b33bf6523fbf931ef24e996e0f1a5
moeabdol <admin.r99@gmail.com>
2015-03-22 22:17:03 +0300

    Implemented flip_to_queen_tile() and flip_to_empty_tile() methods in Tile class to flip any given tile from an empty tile to a queen tile or vice-versa.

--- tile.py
+++ tile.py
@@ -18,3 +18,17 @@ class Tile(urwid.GridFlow):
                 else:
                     cells.append(urwid.Text(('empty_white', u'\u0020')))
         super(Tile, self).__init__(cells, cell_width=1, h_sep=0, v_sep=0, align='left')
+
+    def flip_to_queen_tile(self):
+        self.queen = True
+        if self.color == 'black':
+            self.cells[8] = urwid.Text(('queen_on_black', u'\u265B'), 'center')
+        elif self.color == 'white':
+            self.cells[8] = urwid.Text(('queen_on_white', u'\u265B'), 'center')
+
+    def flip_to_empty_tile(self):
+        self.queen = False
+        if self.color == 'black':
+            self.cells[8] = urwid.Text(('empty_black', u'\u0020'), 'center')
+        elif self.color == 'white':
+            self.cells[8] = urwid.Text(('empty_white', u'\u0020'), 'center')
